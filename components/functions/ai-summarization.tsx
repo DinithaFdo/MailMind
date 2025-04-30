5e3dab2490e47c0e8a087c8e50e7f00e5ba4e59d
aweeshathavishanka <aweesha.info@gmail.com>
2025-04-30 14:00:50 +0530

    Update ai-summarization.tsx

--- components/functions/ai-summarization.tsx
+++ components/functions/ai-summarization.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function AISummarization() {
-  return <div>ai-summarization</div>;
+  return (
+    <div>
+      <div>Ai SUmmarization Page</div>
+    </div>
+  );
 }
