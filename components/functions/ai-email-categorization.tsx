c74853816a37e5248041dd657b79e396e253fbd8
aweeshathavishanka <aweesha.info@gmail.com>
2025-04-30 18:24:35 +0530

    Update ai-email-categorization.tsx

--- components/functions/ai-email-categorization.tsx
+++ components/functions/ai-email-categorization.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function AIEmailCategorization() {
-  return <div>ai-email-categorization</div>;
+  return (
+    <div>
+      <div>
+        <h1>AI Email Categorization</h1>
+        <p>Sri Lanka Largest Service Providing Platform</p>
+      </div>
+    </div>
+  );
 }
