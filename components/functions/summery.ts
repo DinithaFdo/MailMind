111d9189618ca3dec06a9388458ee10123be44d7
Dinitha Fernando <info@dinitha.me>
2025-04-30 17:08:25 +0530

    Summery updated

--- components/functions/summery.ts
+++ components/functions/summery.ts
@@ -1,9 +1,12 @@
 const summery = () => {
   return (
     <div>
+        <h1>Summery</h1>
+        <p>Summery of the tasks and reminders will be displayed here.</p>  
       
     </div>
   )
 }
 
 export default summery
+
