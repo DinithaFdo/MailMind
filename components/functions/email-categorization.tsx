89c626b8f1af4e7b1ec76cc5d355033dad01a0c9
aweeshathavishanka <aweesha.info@gmail.com>
2025-04-30 19:25:50 +0530

    create email categorization paragraph component update and recreate one

--- components/functions/email-categorization.tsx
+++ components/functions/email-categorization.tsx
@@ -1,11 +1,5 @@
 import React from "react";
 
 export default function EmailCategorization() {
-  return (
-    <div>
-      <div>Email Categorization</div>
-      <p>Summarize the email and categorize it into different categories.</p>
-      <h1>Email Categorization</h1>
-    </div>
-  );
+  return <div></div>;
 }
